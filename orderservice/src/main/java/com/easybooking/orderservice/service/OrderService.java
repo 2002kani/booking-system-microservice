2035fc9257cade46dba88fd3cf12debbcbe9966c
02Kani <kanikaradag02@gmail.com>
2025-12-15 21:27:51 +0100

    configuring orderservice

--- orderservice/src/main/java/com/easybooking/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/easybooking/orderservice/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.easybooking.orderservice.service;
 
 import com.easybooking.bookingservice.event.BookingEvent;
+import com.easybooking.orderservice.entity.Order;
 
 public interface OrderService {
     void orderEvent(BookingEvent event);
+    Order createOrder(BookingEvent event);
 }
--- orderservice/src/main/java/com/easybooking/orderservice/service/impl/OrderServiceImpl.java
+++ orderservice/src/main/java/com/easybooking/orderservice/service/impl/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package com.easybooking.orderservice.service.impl;
 
 import com.easybooking.bookingservice.event.BookingEvent;
+import com.easybooking.orderservice.client.InventoryServiceClient;
+import com.easybooking.orderservice.entity.Order;
+import com.easybooking.orderservice.repository.OrderRepository;
 import com.easybooking.orderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,10 +12,28 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
+    private OrderRepository orderRepository;
+    private InventoryServiceClient inventoryServiceClient;
 
     @KafkaListener(topics = "booking", groupId = "order-service")
     @Override
     public void orderEvent(BookingEvent event) {
         log.info("Order Event received: {}", event);
+
+        Order order = createOrder(event);
+        orderRepository.saveAndFlush(order);
+
+        inventoryServiceClient.updateInventory(order.getEventId(), order.getTicketCount());
+        log.info("Order Event sent to inventory: {}, less tickets: {}", order.getEventId(), order.getTicketCount());
+    }
+
+    @Override
+    public Order createOrder(BookingEvent event) {
+        return Order.builder()
+                .customerId(event.getUserId())
+                .eventId(event.getEventId())
+                .ticketCount(event.getTicketCount())
+                .totalPrice(event.getTotalPrice())
+                .build();
     }
 }
