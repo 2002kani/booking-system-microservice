48f7d792c70665c9af0869830e2aa20a5352dc09
02Kani <kanikaradag02@gmail.com>
2025-12-20 20:29:30 +0100

    adding swagger doc

---
+++ bookingservice/src/main/java/com/easybooking/bookingservice/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.easybooking.bookingservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceApi() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Booking Service API")
+                        .description("Complete Booking Service API by Kani Karadag")
+                        .version("v1.0.0"));
+    }
+}
